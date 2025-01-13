1eb071650ebbe26953c9aed63fe3560b558e3244
your username <tmarina@eafit.edu.co>
2025-01-13 00:54:48 -0500

    add port

--- ymca/wsgi.py
+++ ymca/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ymca.settings')
 
 application = get_wsgi_application()
+
+port = os.getenv('PORT', 8000)
