9dcd7944c8b433b5c8c08063a68a848b66fdfb3f
your username <tmarina@eafit.edu.co>
2025-01-13 01:02:10 -0500

    add unicorn

--- ymca/wsgi.py
+++ ymca/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ymca.settings')
 
 application = get_wsgi_application()
-
-port = os.getenv('PORT', 8000)
